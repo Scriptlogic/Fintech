cc2eecde07e93389e7e195150f70d6437a5d13e3
Siddhi Nilkanth <siddhi0676@gmail.com>
2026-05-05 15:52:57 +0530

    fix API baseURL

--- mern-app/client/src/api/axios.js
+++ mern-app/client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL:         '/api',
-  timeout:         10000,
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
